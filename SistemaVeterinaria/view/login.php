190b67ac8ef12d93892c7b4d8713bbef1293d2ed
Chiriguaya33 <94765768+Chiriguaya33@users.noreply.github.com>
2024-05-24 18:47:03 -0500

    Add files via upload

---
+++ SistemaVeterinaria/view/login.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>login</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/css/all.min.css">
+
+</head>
+
+<body>
+    
+    <img class="wave"src="../img/wave.png" alt="">
+
+    <div class="contenedor">
+        <div class="img">
+            <img src="../img/consultorio.svg" alt="">
+        </div>
+        <div class="contenido-login">
+            <form action="">
+                <img src="../img/veterinario.png" alt="">
+                <h2>Veterinaria</h2>
+                <div class="input-div cedula">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>CEDULA</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>CONTRASEÑA</h5>
+                        <input type="pass" name="pass" class="input">
+                    </div>
+                </div>
+                <a href="">Created Warpiece</a>
+                <input type="submit" class="btn" value="Iniciar Sesion">
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
