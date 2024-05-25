07a3f5077a341ca14e3e5afda4f314dd139d6f80
Chiriguaya33 <chiriguaya11a@gmail.com>
2024-05-24 19:01:57 -0500

    Login

--- SistemaVeterinaria/view/login.php
+++ SistemaVeterinaria/view/login.php
@@ -23,35 +23,51 @@
     <img class="wave"src="../img/wave.png" alt="">
 
     <div class="contenedor">
+
         <div class="img">
             <img src="../img/consultorio.svg" alt="">
         </div>
+
         <div class="contenido-login">
+
             <form action="">
+
                 <img src="../img/veterinario.png" alt="">
-                <h2>Veterinaria</h2>
+                <h2>VETERINARIA</h2>
+
                 <div class="input-div cedula">
+
                     <div class="i">
                         <i class="fas fa-user"></i>
                     </div>
+
                     <div class="div">
                         <h5>CEDULA</h5>
                         <input type="text" name="user" class="input">
                     </div>
+
                 </div>
+
                 <div class="input-div pass">
-                <div class="i">
+
+                    <div class="i">
                         <i class="fas fa-lock"></i>
                     </div>
+
                     <div class="div">
                         <h5>CONTRASEÑA</h5>
                         <input type="pass" name="pass" class="input">
                     </div>
+                    
                 </div>
+
                 <a href="">Created Warpiece</a>
                 <input type="submit" class="btn" value="Iniciar Sesion">
+
             </form>
+
         </div>
+
     </div>
 
 </body>
